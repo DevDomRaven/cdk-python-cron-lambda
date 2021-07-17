8ac2c7d4deff3a02f8cdf424e9bdc197df4a51d8
Dominic <dominic@Dominics-MacBook-Pro.local>
2021-07-17 22:07:41 +0100

    Init commit for simple lambda that deploys with event

--- lambda_deploy/lambda_deploy_stack.py
+++ lambda_deploy/lambda_deploy_stack.py
@@ -5,11 +5,24 @@ from aws_cdk import core as cdk
 # with examples from the CDK Developer's Guide, which are in the process of
 # being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import core
-
+import aws_cdk.aws_lambda as lambda_
+from aws_cdk.aws_lambda_python import PythonFunction
+from aws_cdk.aws_events import Rule, Schedule
+from aws_cdk.aws_events_targets import LambdaFunction
 
 class LambdaDeployStack(cdk.Stack):
 
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        function = PythonFunction(self, "MyFunction",
+            entry="./lambda_deploy/functions/", # required
+            handler="handler", # optional, defaults to 'handler'
+            runtime=lambda_.Runtime.PYTHON_3_6
+        )
+        lambda_target = LambdaFunction(function)
+
+        Rule(self, "ScheduleRule",
+            schedule=Schedule.cron(minute="0", hour="1"),
+            targets=[lambda_target]
+        )
